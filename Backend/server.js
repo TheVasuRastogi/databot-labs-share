dee7bdee8beac4322965a87e508e90310aed3a21
Vasu Rastogi <vasurastogi213@gmail.com>
2025-08-11 18:38:57 +0530

    Update CORS configuration

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', process.env.FRONTEND_URL],
+    origin: ['http://localhost:3000', 'http://localhost:3001', process.env.FRONTEND_URL, process.env.CORS_ORIGIN],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
